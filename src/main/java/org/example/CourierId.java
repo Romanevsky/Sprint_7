16aeb8d5289d4a510e163d32bce0d661aac391ec
Roman Valiulin <v9139493364@yandex.ru>
2023-08-02 10:09:38 +0700

    commit news!!!

---
+++ src/main/java/org/example/CourierId.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class CourierId {
+    private String id;
+
+    public CourierId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
